1db635cdf7f47045c54a95b9e10e648d887e778a
Galib12345 <mchowdhury2022@elam.co.uk>
2022-11-15 12:41:23 +0000

    I made my phsics and trying my jump animation

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     GameObject cam;
-    private void Start()
+    Rigidbody myRigidbody;
+    void Start()
     {
         cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
     }
     
     
@@ -17,9 +19,23 @@ public class CharacterController : MonoBehaviour
     float rotation = 0.0f;
     float camRotation = 0.0f;
     
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce = 300.0f;
+
     void Update()
     {
-        transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxspeed);
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.AddForce(transform.up * jumpForce);
+        }
+        
+        //transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxspeed);
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxspeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
 
         rotation = rotation + Input.GetAxis("Mouse X");
         transform.rotation = Quaternion.Euler(new Vector3(0.0f,rotation,0.0f));
