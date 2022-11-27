52dc902082350171cd3f50e4c9e62e44c59dc5ad
Galib12345 <mchowdhury2022@elam.co.uk>
2022-11-27 18:54:33 +0000

    table

---
+++ Assets/scripts/Spawning.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+    public GameObject ball;
+    public float spawnTime = 3f;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("SpawnBall", spawnTime, spawnTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    void SpawnBall()
+    {
+        var newBall = GameObject.Instantiate(ball);
+    }
+}
